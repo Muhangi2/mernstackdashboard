9b8405b540ab262bb41deefca8f8fd412962d721
Muhangi2 <eliodamutiba@gmail.com>
2023-12-30 20:25:04 +0300

    stringfy

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,8 @@
 import User from "../models/User.js";
 import productStats from "../models/productStats.js";
 import products from "../models/products.js";
+import * as flatted from "flatted";
+
 
 export const getProducts=async(req,res)=>{
     try {
@@ -23,8 +25,10 @@ export const getProducts=async(req,res)=>{
 }
 export const getCustomers=async(req,res)=>{
   try {
-   const customers=User.find({role:"user"}).select("-password");
-     res.status(200).json(customers);
+    const customers = await User.find({ role: "user" }).select("-password");
+
+    res.status(200).json(customers);
+    
 
   } catch (error) {
        res.status(400).json({message:error.message});
--- server/index.js
+++ server/index.js
@@ -1,48 +1,56 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import mongoose, { Mongoose } from "mongoose"
-import helmet from "helmet"
-import  morgan from "morgan"
-import bodyParser from "body-parser"
-// import  clientRoute  from "./routes/client.js"
-import { salesRoute } from "./routes/sales.js"
-import { managementRoute } from "./routes/management.js"
-import generalRoute from "./routes/general.js"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
 
-//insserting data into database
-import products from "./models/products.js"
-import productStats from "./models/productStats.js"
-import  {dataProduct, dataProductStat} from "./data/data.js"
-import clientRoute from "./routes/client.js"
+// import clientRoute from "./routes/client.js";
+import { salesRoute } from "./routes/sales.js";
+import { managementRoute } from "./routes/management.js";
+import generalRoute from "./routes/general.js";
 
+// Inserting data into the database
+import products from "./models/products.js";
+import productStats from "./models/productStats.js";
+import { dataProduct, dataProductStat } from "./data/data.js";
+import clientRoute from "./routes/client.js";
 
-dotenv.config()
-const app =express();
+dotenv.config();
+
+const app = express();
+
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
-app.use(morgan("common"))
-app.use(bodyParser.json())
-
+app.use(morgan("common"));
+app.use(cors()); // Simplified CORS configuration
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 
-// routes
-app.use("/client",clientRoute)
-app.use("/general",generalRoute)
-app.use("/sales",salesRoute)
-app.use("/management",managementRoute)
+// Routes
+app.use("/client", clientRoute);
+app.use("/general", generalRoute);
+app.use("/sales", salesRoute);
+app.use("/management", managementRoute);
 
-//CONFIGURING THE CONNECTION WITH DATABASE AND PORT
+// Configuring the connection with the database and port
+const PORT = process.env.PORT || 8000;
 
-const PORT=process.env.PORT||8000
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-mongoose.connect(process.env.MONGODB_URL,{
-    useNewUrlParser:true,
-   
-}).then(()=>{app.listen(PORT,()=>console.log(`Server Port:${PORT}`))
-//adding data once
-// products.insertMany(dataProduct)
-// productStats.insertMany(dataProductStat)
-}).catch((error)=>console.log(`${error} did not connect`))
+    // Adding data once
+    // products.insertMany(dataProduct);
+    // productStats.insertMany(dataProductStat);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
